58e8ba9dc66f145bba21db6928d2bca9267bea99
Oleksandr Vlasov <drumlife182@gmail.com>
2023-05-20 20:38:00 +0200

    Worked with methods in C#

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,28 +1,41 @@
 ﻿namespace Classes
 {
-    public class Person
+    partial class Program
     {
-        public string? Name;
 
-        public void Introduce(string to)
-        {
-            Console.WriteLine("Hi {0}, I am {1}", to, Name);
-        }
-
-        public static Person Parse(string str)
-        {
-            var person = new Person();
-            person.Name = str;
-
-            return person;
-        }
-    }
-    class Program
-    {
         static void Main(string[] args)
         {
-            var person = Person.Parse("John");
-            person.Introduce("Mosh");
+            int number;
+            var result = int.TryParse("abc", out number);
+            if (result)
+                Console.WriteLine(number);
+            else
+                Console.WriteLine("Conversion failled");
+        }
+
+        static void UseParams()
+        {
+            var calculator = new Calculator();
+            Console.WriteLine(calculator.Add(1, 2));
+            Console.WriteLine(calculator.Add(1, 2, 3));
+            Console.WriteLine(calculator.Add(1, 2, 3, 4));
+            Console.WriteLine(calculator.Add(new int[] { 1, 2, 3, 4, 5 }));
+        }
+
+        static void UsePoints()
+        {
+            try
+            {
+                var point = new Point(10, 20);
+                point.Move(null);
+                Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
+                point.Move(new Point(100, 200));
+                Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An unexpected error occured.");
+            }
         }
     }
 }
